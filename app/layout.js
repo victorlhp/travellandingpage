fc21d68b4e1adcbe3a7d4f7c5b450a2dcf8197ce
bgsdo1234 <victor270196@gmail.com>
2023-08-31 16:36:59 -0300

    cor , styles e mudança layout

--- app/layout.js
+++ app/layout.js
@@ -1,13 +1,31 @@
 import './globals.css'
+import styles from './styles.module.css'
+// import { Quicksand } from 'next'
 
 export const metadata = {
   title: 'Travel Landing Page',
 }
 
+// const quicksand = Quicksand({
+
+// })
+
+function Logo() {
+  return (
+    <p className={styles.logo}>Ready<span className={styles.comercial}>&</span>Go</p>
+  );
+}
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <header>
+          <Logo/>
+        </header>
+        <main>
+          {children}
+        </main>
+        </body>
     </html>
   )
 }
