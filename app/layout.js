8e2af0f7de925898469340c42ef02714db12499d
bgsdo1234 <victor270196@gmail.com>
2023-10-03 15:21:46 -0300

    adiciona titulo e subtitulo

--- app/layout.js
+++ app/layout.js
@@ -32,13 +32,28 @@ function Menu({children, x, y}) {
   );
 }
 
+function Titulo({x, y}) {
+  return (
+    <h1 style={{left: x, top: y}}className={styles.titulo}>Your perfect 
+    honeymoon</h1>
+  );
+}
+
+function Sub({x, y}) {
+  return (
+    <h2 style={{left: x, top: y}}className={styles.sub}> 
+    Mayami is more than a honeymoon destination – it’s a perfect romantic spot for couples who like  spending time in beach. </h2>
+  );
+}
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
         <header>
+          
           <Logo x={135} y={53}/>
-        <Menu x={435} y={60}> 
+          <Menu x={435} y={60}> 
           <ItemMenu nome="Home"/>
           <ItemMenu nome="About"/>
           <ItemMenu nome="Contact"/>
@@ -47,7 +62,9 @@ export default function RootLayout({ children }) {
         </Menu>
         </header>
         <main>
+        <Titulo x={135} y={238}/>
           {children}
+        <Sub x={136} y={465.04}/>  
         </main>
         </body>
     </html>
