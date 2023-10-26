9d381764b61863d84a0a214144e770c8b9675277
bgsdo1234 <victor270196@gmail.com>
2023-10-26 15:41:18 -0300

    adiciona as redes sociais

--- app/layout.js
+++ app/layout.js
@@ -1,84 +1,91 @@
 import './globals.css'
 import styles from './styles.module.css'
-// import { Quicksand } from 'next'
+// import { Quicksand } from 'next/font/google'
+import Image from 'next/image';
+import facebook from '../public/facebook.svg'
+import twitter from '../public/twitter.svg'
+import linkedin from '../public/linkedin.svg'
+
 
 export const metadata = {
   title: 'Travel Landing Page',
 }
 
 // const quicksand = Quicksand({
-
+//   subsets: ['latin'],
+//   display: 'swap',
 // })
 
-function Logo({x, y}) {
+function Logo({x, y}){
   return (
-    <p style={{left: x, top: y}}className={styles.logo}>Ready<span className={styles.comercial}>&</span>Go</p>
+    <p style={{left: x, top: y }} className={styles.logo}>Ready<span className={styles.comercial}>&</span>Go</p>
   );
 }
 
-function ItemMenu({nome}) {
-    return (
-      <p className={styles.itemMenu}>{nome}</p>
-    );
+function ItemMenu({nome}){
+  return (
+    <p className={styles.itemMenu}>{nome}</p>
+  );
 }
 
-function Menu({children, x, y}) {
+function Menu({children, x, y}){
   return(
     <ul style={{left: x, top: y}} className={styles.menu}>
       {children.map((itemMenu) => (
-        <li key={itemMenu.nome} > {itemMenu}</li>
+        <li key={itemMenu.nome}>{itemMenu}</li>
       ))}
     </ul>
   );
 }
 
-function Contato({telefone}) {
+function Icon({x, y}) {
   return (
-    <div className={styles.contato}>
-      <img src='phone icon.png' alt='Phone'/>
-      <span>{telefone}</span>
+    <p style={{left: x, top:y}} className={styles.icon}><Image src='/phone_icon.png' width={19} height={19}/></p>
+  );
+  
+}
+
+function Contato({x, y}) {
+  return (
+    <p style={{left: x, top:y}} className={styles.contato}> +38(0978849989)</p>
+  );
+  
+}
+
+function RedesSociais({x, y}) {
+  return (
+    <div style={{left: x, top: y}} className={styles.redesSociais}>
+      <Image src={facebook}/>
+      <Image src={twitter}/>
+      <Image src={linkedin}/>
     </div>
-  );
-}
-
-function Titulo({x, y}) {
-  return (
-    <h1 style={{left: x, top: y}}className={styles.titulo}>Your perfect 
-    honeymoon</h1>
-  );
-}
-
-function Sub({x, y}) {
-  return (
-    <h2 style={{left: x, top: y}}className={styles.sub}> 
-    Mayami is more than a honeymoon destination – it’s a perfect romantic spot for couples who like  spending time in beach. </h2>
-  );
+  )
 }
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
-        <header className={styles.cabecalho}>
-          
+        <header>
           <Logo x={135} y={53}/>
-          <Menu x={435} y={60}> 
-          <ItemMenu nome="Home"/>
-          <ItemMenu nome="About"/>
-          <ItemMenu nome="Contact"/>
-          <ItemMenu nome="Blog"/>
-          <ItemMenu nome="Videos"/>
-          
-        </Menu>
+          <Menu x={435} y={60}>
+            <ItemMenu nome="Home"/>
+            <ItemMenu nome="About"/>
+            <ItemMenu nome="Contact"/>
+            <ItemMenu nome="Blog"/>
+            <ItemMenu nome="Videos"/>
+          </Menu>
 
-        <Contato className={styles.contato} telefone={'+38(097)8849989'}/>
+          <Icon></Icon>
+          <Contato></Contato>
         </header>
         <main>
-        <Titulo x={135} y={238}/>
           {children}
-        <Sub x={136} y={465.04}/>  
         </main>
-        </body>
+        <footer>
+          <RedesSociais x={1175} y={843}/>
+        </footer>
+      </body>
     </html>
   )
 }
